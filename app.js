8e7f136a85cdebe309c4e401491cc91a517c0834
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-03-30 14:05:46 -0400

    Second Commit

--- app.js
+++ app.js
@@ -7,16 +7,73 @@ const playerImage = new Image();
 playerImage.src = 'https://www.frankslaboratory.co.uk/downloads/shadow_dog.png'
 const spriteWidth = 575; //image is 6876px widtd, 12 columns
 const spriteHeight = 523;    // height is 5230px and 10 rows
-
+let frameX = 0;
+let frameY = 1;
+let gameFrame = 0;
+const staggerFrames = 5; //controls speed of animation. Higher num slows it down
+const spriteAnimations = []; //empty array that contains data for all of my animations
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7, 
+    },
+    {
+        name: 'jump',
+        frames: 7, 
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],  
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;  
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(animationStates);
 
 function animate() {
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT );
-    // ctx.fillRect(100,50,100,100);
-    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh );
-    ctx.drawImage(playerImage, 1 * spriteWidth, 0, spriteWidth, spriteHeight, 0,0, spriteWidth, spriteHeight);
+   let position = Math.floor(gameFrame/staggerFrames) % 6;
+   frameX = spriteWidth * position;
+   ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0,0, spriteWidth, spriteHeight);
+      
+    gameFrame++;
     requestAnimationFrame(animate);
 
 
     
 };
 animate();
+
+// //////////
+// spriteAnimations = [
+//     'idle' = {
+//         width: 525,
+//         height: 523,
+//         loc: [
+//             { x: 0, y: 0 },
+//             { x: 575, y: 0 },
+//             { x: 1150, y: 0 },
+//             { x: 1725, y: 0 },
+//             { x: 2300, y: 0 },
+//              { x: 2875, y: 0 },
+//              { x: 3450, y: 0 },
+      
+//         ]
+//     },
+
+//     'jump' = {
+//         width: 120,
+//         height: 120,
+//         loc: []
+//     },
+//     'run' = {
+//         width: 1200,
+//         height: 1250,
+//         loc: []
+//     }
+// ];
