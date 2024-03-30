4f4fbfce9eac5f2646571fb0388f074eb58dc3fb
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-03-30 15:03:42 -0400

    Third Commit

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let playerState = 'run';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+    })
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
@@ -7,19 +13,49 @@ const playerImage = new Image();
 playerImage.src = 'https://www.frankslaboratory.co.uk/downloads/shadow_dog.png'
 const spriteWidth = 575; //image is 6876px widtd, 12 columns
 const spriteHeight = 523;    // height is 5230px and 10 rows
-let frameX = 0;
-let frameY = 1;
 let gameFrame = 0;
 const staggerFrames = 5; //controls speed of animation. Higher num slows it down
 const spriteAnimations = []; //empty array that contains data for all of my animations
 const animationStates = [
     {
-        name: 'idle',
+        name: 'idle',  //make sure there's a comma between each object
         frames: 7, 
     },
     {
         name: 'jump',
         frames: 7, 
+    }, 
+    {
+        name: 'fall', //was blinking when set at 9. 7 is the correct num of frames
+        frames: 7, 
+    }, 
+    {
+        name: 'run',
+        frames: 9, 
+    }, 
+    {
+        name: 'dizzy',
+        frames: 11, 
+    }, 
+    {
+        name: 'sit',
+        frames: 5, 
+    }, 
+    {
+        name: 'roll',
+        frames: 7, 
+    }, 
+    {
+        name: 'bite',
+        frames: 7, 
+    }, 
+    {
+        name: 'ko',
+        frames: 12, 
+    }, 
+    {
+        name: 'getHit',
+        frames: 4, 
     }
 ];
 animationStates.forEach((state, index) => {
@@ -37,43 +73,13 @@ console.log(animationStates);
 
 function animate() {
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT );
-   let position = Math.floor(gameFrame/staggerFrames) % 6;
-   frameX = spriteWidth * position;
-   ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0,0, spriteWidth, spriteHeight);
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+   
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0,0, spriteWidth, spriteHeight);
       
     gameFrame++;
     requestAnimationFrame(animate);
-
-
-    
 };
 animate();
-
-// //////////
-// spriteAnimations = [
-//     'idle' = {
-//         width: 525,
-//         height: 523,
-//         loc: [
-//             { x: 0, y: 0 },
-//             { x: 575, y: 0 },
-//             { x: 1150, y: 0 },
-//             { x: 1725, y: 0 },
-//             { x: 2300, y: 0 },
-//              { x: 2875, y: 0 },
-//              { x: 3450, y: 0 },
-      
-//         ]
-//     },
-
-//     'jump' = {
-//         width: 120,
-//         height: 120,
-//         loc: []
-//     },
-//     'run' = {
-//         width: 1200,
-//         height: 1250,
-//         loc: []
-//     }
-// ];
